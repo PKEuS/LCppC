7221fa4e15a48ee939014413c3df6cfd15ce5f51
PKEuS <philipp@pkeus.de>
2020-06-20 13:30:29 +0200

    Improved CheckStl::string_c_str(): - New performance check for assigning std::string variables - Fixed false negatives in CheckStl::string_c_str() by supporting more generic expressions

--- lib/checkstl.h
+++ lib/checkstl.h
@@ -194,6 +194,7 @@ private:
     void string_c_strThrowError(const Token* tok);
     void string_c_strError(const Token* tok);
     void string_c_strReturn(const Token* tok);
+    void string_c_strAssign(const Token* tok);
     void string_c_strParam(const Token* tok, unsigned int number);
 
     void outOfBoundsError(const Token *tok, const std::string &containerName, const ValueFlow::Value *containerSize, const std::string &index, const ValueFlow::Value *indexValue);
@@ -252,6 +253,7 @@ private:
         c.checkFindInsertError(nullptr);
         c.string_c_strError(nullptr);
         c.string_c_strReturn(nullptr);
+        c.string_c_strAssign(nullptr);
         c.string_c_strParam(nullptr, 0);
         c.string_c_strThrowError(nullptr);
         c.sizeError(nullptr);
