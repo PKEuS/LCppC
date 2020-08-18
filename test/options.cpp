2d620716d317d1737f5d552ac086fab0d418539f
PKEuS <philipp@pkeus.de>
2020-08-18 21:31:28 +0200

    Adapted copyright notice to the requirements of LCppC

--- test/options.cpp
+++ test/options.cpp
@@ -1,6 +1,6 @@
 // LCppC - A tool for static C/C++ code analysis
 // Copyright (C) 2007-2019 Cppcheck team.
-/ Copyright (C) 2020 LCppC project.
+// Copyright (C) 2020 LCppC project.
 //
 // This program is free software: you can redistribute it and/or modify
 // it under the terms of the GNU General Public License as published by
